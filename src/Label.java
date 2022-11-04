45ef93a5d64213f64f4dcb86cb9b1c0399b04d7f
Miiichiiii <bue.mich38@gmail.com>
2022-11-04 17:30:20 +0100

    Added Enum to represent the State of the Label

--- src/Label.java
+++ src/Label.java
@@ -1,7 +1,19 @@
 import javax.swing.*;
 
 public class Label {
-    public Label(JLabel label) {
-
+    protected State state;
+    protected JLabel label;
+    public Label(JLabel label, State state) {
+        this.state = state;
+        this.label = label;
     }
+
+    public State getState() {
+        return this.state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
 }
