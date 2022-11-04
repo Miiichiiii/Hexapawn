ae40139f41df546647a44780d45bc1caf36758b3
Miiichiiii <bue.mich38@gmail.com>
2022-11-04 17:44:04 +0100

    Added a retrieveLabel function

--- src/Label.java
+++ src/Label.java
@@ -1,13 +1,26 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class Label {
+    protected static ArrayList<JLabel> JlabelList = new ArrayList<JLabel>();
+    protected static ArrayList<Label> labelList = new ArrayList<Label>();
     protected State state;
     protected JLabel label;
     public Label(JLabel label, State state) {
         this.state = state;
         this.label = label;
+        JlabelList.add(label);
+        labelList.add(this);
     }
 
+    public static Label retrieveLabel(JLabel label) {
+        for(int i = 0; i < JlabelList.size(); i++) {
+            if (JlabelList.get(i) == label) {
+                return labelList.get(i);
+            }
+        }
+        return null;
+    }
     public State getState() {
         return this.state;
     }
