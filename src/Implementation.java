83058e67e10f0f3e06720211104a26c39159987f
Miiichiiii <bue.mich38@gmail.com>
2022-11-04 16:37:27 +0100

    Added label Listeners

--- src/Implementation.java
+++ src/Implementation.java
@@ -1,5 +1,29 @@
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class Implementation extends GUI {
     public Implementation() {
         super();
+    }
+
+    private void initializeListener() {
+        //For every label in the label list:
+        //Add a ActionListener and run the onClick function
+        for(int y = 0; y < labelList.length; y++) {
+            for (int x = 0; x < labelList[0].length; x++) {
+                labelList[y][x].addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        onClick(e);
+                        super.mouseClicked(e);
+                    }
+                });
+            }
+        }
+
+    }
+
+    private void onClick(MouseEvent e) {
+
     }
 }
