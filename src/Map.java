dcc261951f288b48e3e626502e65fa177d50be6e
MoritzMy <moritzmeyer52@gmail.com>
2022-11-04 18:47:16 +0100

    Added state check, added MapState

--- src/Map.java
+++ src/Map.java
@@ -2,15 +2,23 @@ import javax.swing.*;
 
 public class Map {
     public void MapState() {
-        State currentState = State.EMPTY;
+
+
+
         boolean turn = true; // true = whites turn / false = blacks turn
 
         State[][] stateMap = new State[3][3];
+
         for(int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 stateMap[i][j] = Label.labelList.get(i*3+j).getState();
 
             }
         }
+
+        /*stateMap = state_0_0, state_0_1, state_0_2
+                     state_1_0, state_1_1, state_1_2
+                     state_2_0, state_2_1, state_2_2
+         */
     }
 }
