085b447923607f9496aa7b39e63b7d6bb88756c5
Miiichiiii <bue.mich38@gmail.com>
2022-11-04 18:37:34 +0100

    Added a clearColor function

--- src/Map.java
+++ src/Map.java
@@ -2,9 +2,8 @@ import javax.swing.*;
 
 public class Map {
     public void MapState() {
-        boolean turn = 0 // 0 = whites turn / 1 = blacks turn
-        for(int i = 0; i <Label.labelList.size(); i++) {
-            Label.
+        //boolean turn = 0 // 0 = whites turn / 1 = blacks turn
+        //for(int i = 0; i <Label.labelList.size(); i++) {
+        //    Label.
         }
     }
-}
