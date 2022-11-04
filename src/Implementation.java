f3652bde553d69b9a057df6559548ef372bfb818
Miiichiiii <bue.mich38@gmail.com>
2022-11-04 17:17:21 +0100

    Loading images

--- src/Implementation.java
+++ src/Implementation.java
@@ -1,10 +1,18 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Implementation extends GUI {
     public Implementation() {
         super();
         initializeListener();
+        if(!initializePictures()) {
+            System.out.println("Something went wrong with initializing the pictures");
+        }
     }
 
     private void initializeListener() {
@@ -26,5 +34,35 @@ public class Implementation extends GUI {
 
     private void onClick(MouseEvent e) {
 
+    }
+
+    private boolean initializePictures() {
+        boolean works;
+        for (int x = 0; x < labelList[0].length; x++) {
+            works = load_image(labelList[0][x], "pictures/pawn_black.png");
+            if (!works) return false;
+        }
+        for (int x = 0; x < labelList[1].length; x++) {
+            works = load_image(labelList[1][x], "pictures/empty_field.png");
+            if (!works) return false;
+        }
+        for (int x = 0; x < labelList[2].length; x++) {
+            works = load_image(labelList[2][x], "pictures/pawn_white.png");
+            if (!works) return false;
+        }
+        return true;
+    }
+
+    private boolean load_image(JLabel label, String path) {
+        //Catch the Exception if something went wrong with loading the image
+        try {
+            BufferedImage image = ImageIO.read(new File(path)); //Read the image
+            ImageIcon icon = new ImageIcon(image); //Convert the image to an icon
+            label.setIcon(icon); //Set the picture to the label
+        }
+        catch (IOException ex) {
+            return false;
+        }
+        return true;
     }
 }
