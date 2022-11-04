2eb9a05414c3851a9833ba3acb59e2ef9f6436f0
Miiichiiii <bue.mich38@gmail.com>
2022-11-04 16:00:02 +0100

    Added Labels

--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class GUI {
+    private JLabel label_0_0;
+    private JLabel label_0_1;
+    private JLabel label_0_2;
+    private JLabel label_1_0;
+    private JLabel label_1_1;
+    private JLabel label_1_2;
+    private JLabel label_2_0;
+    private JLabel label_2_1;
+    private JLabel label_2_2;
 }
