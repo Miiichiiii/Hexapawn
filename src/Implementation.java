ecb54ad6816f7fbc7a7c6c55dc93b30e1a7d5946
Miiichiiii <bue.mich38@gmail.com>
2022-11-05 20:57:04 +0100

    Added turn logic, Added black move

--- src/Implementation.java
+++ src/Implementation.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class Implementation extends GUI {
+    public static boolean turn = true;
     public Implementation() {
         super();
         initializeListener();
@@ -45,6 +46,55 @@ public class Implementation extends GUI {
         }
     }
     public void onClick(MouseEvent e) {
+        if (turn) whiteMove(e);
+        else blackMove(e);
+    }
+
+    public void blackMove(MouseEvent e) {
+        JLabel Current_Label = (JLabel) e.getComponent();
+        Label LabelObj = Label.retrieveLabel(Current_Label);
+        assert LabelObj != null; //IntelliJ cries without this
+
+        clearColor();
+        if (LabelObj.getMove() == Move.NEW_POSITION) { //If Label is set to NEW_POSITION the pawn can move to this field
+            LabelObj.setState(State.BLACK); // Set the state of an empty field to black, indicate move
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (Label.retrieveLabel(labelList[i][j]).getMove() == Move.SELECTED) { // Search for the pawn which will move to the new Label
+                        Label.retrieveLabel(labelList[i][j]).setState(State.EMPTY); // Replace with Empty Field
+                        loadImage(labelList[i][j], Picture.EMPTY); //Load empty image for the old pawn position
+                        loadImage(Current_Label, Picture.BLACK); //Load the white pawn picture for the new pawn position
+                        Label.clearMove(); //clear every Move state
+                        turn = !turn; //Other player's turn
+                        return;
+                    }
+                }
+            }
+        }
+        Label.clearMove(); //clear every Move state
+        // If the field has a black pawn, allow moves
+        if (LabelObj.getState() == State.BLACK) {
+            Current_Label.setBackground(Color.GRAY); //Indicate that the field is selected
+            LabelObj.setMove(Move.SELECTED); //Set the move state to selected
+            short targetRow = (short) (LabelObj.y + 1); //The row in which the pawn can potentially move
+            if (targetRow > 2) return; // Avoid index out of bounds exception. Maybe win here TODO.
+            if (Label.retrieveByCoordinates(LabelObj.x, targetRow).getState() == State.EMPTY) { //Only allow forward move if field is empty
+                labelList[targetRow][LabelObj.x].setBackground(Color.GREEN); //Indicate a possible move
+                Label.retrieveByCoordinates(LabelObj.x, targetRow).setMove(Move.NEW_POSITION); //Set the move state to selected
+            }
+            if (LabelObj.x + 1 < 3 &&  Label.retrieveByCoordinates((short)(LabelObj.x + 1), targetRow).getState() == State.WHITE) { //Only allow diagonal move if there is a black pawn
+                labelList[targetRow][LabelObj.x + 1].setBackground(Color.GREEN); //Indicate a possible move
+                Label.retrieveByCoordinates((short)(LabelObj.x + 1), targetRow).setMove(Move.NEW_POSITION); //Set the move state to selected
+            }
+            if (LabelObj.x - 1 >= 0 && Label.retrieveByCoordinates((short)(LabelObj.x - 1), targetRow).getState() == State.WHITE) { //Only allow diagonal move if there is a black pawn
+                labelList[targetRow][LabelObj.x - 1].setBackground(Color.GREEN); //Indicate a possible move
+                Label.retrieveByCoordinates((short)(LabelObj.x - 1), targetRow).setMove(Move.NEW_POSITION); //Set the move state to selected
+            }
+
+        }
+    }
+
+    public void whiteMove(MouseEvent e) {
         JLabel Current_Label = (JLabel) e.getComponent();
         Label LabelObj = Label.retrieveLabel(Current_Label);
         assert LabelObj != null; //IntelliJ cries without this
@@ -59,16 +109,17 @@ public class Implementation extends GUI {
                         loadImage(labelList[i][j], Picture.EMPTY); //Load empty image for the old pawn position
                         loadImage(Current_Label, Picture.WHITE); //Load the white pawn picture for the new pawn position
                         Label.clearMove(); //clear every Move state
+                        turn = !turn; //Other player's turn
                         return;
                     }
                 }
             }
         }
         Label.clearMove(); //clear every Move state
-        Current_Label.setBackground(Color.GRAY); //Indicate that the field is selected
-        LabelObj.setMove(Move.SELECTED); //Set the move state to selected
         // If the field has a white pawn, allow moves
         if (LabelObj.getState() == State.WHITE) {
+            Current_Label.setBackground(Color.GRAY); //Indicate that the field is selected
+            LabelObj.setMove(Move.SELECTED); //Set the move state to selected
             short targetRow = (short) (LabelObj.y - 1); //The row in which the pawn can potentially move
             if (targetRow < 0) return; // Avoid index out of bounds exception. Maybe win here TODO.
             if (Label.retrieveByCoordinates(LabelObj.x, targetRow).getState() == State.EMPTY) { //Only allow forward move if field is empty
@@ -84,8 +135,7 @@ public class Implementation extends GUI {
                 Label.retrieveByCoordinates((short)(LabelObj.x - 1), targetRow).setMove(Move.NEW_POSITION); //Set the move state to selected
             }
 
-
-            }
+        }
 
     }
 
