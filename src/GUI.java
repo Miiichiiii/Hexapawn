4ffdee6322d34206eb4a9548cc572a75477e39f5
Miiichiiii <bue.mich38@gmail.com>
2022-11-06 13:12:23 +0100

    Gui changes and pictures for win

--- src/GUI.java
+++ src/GUI.java
@@ -11,6 +11,10 @@ public class GUI extends JFrame{
     protected JLabel label_2_1;
     protected JLabel label_2_2;
     protected JPanel mainPanel;
+    private JButton newGameButton;
+    private JCheckBox computerCheckBox;
+    private JTextArea textArea1;
+    private JLabel winnerLabel;
     public JLabel[][] labelList = {{label_0_0, label_0_1, label_0_2},
                                    {label_1_0, label_1_1, label_1_2},
                                    {label_2_0, label_2_1, label_2_2}};
@@ -18,7 +22,7 @@ public class GUI extends JFrame{
     public GUI() {
         setContentPane(mainPanel);
         setTitle("Hexapawn"); //Sets the name of the Application
-        setSize(805, 818);
+        setSize(1150, 818);
         setLocationRelativeTo(null); //Set spawn point to the middle of the screen
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
